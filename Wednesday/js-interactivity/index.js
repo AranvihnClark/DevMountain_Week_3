ce4a6f9753d05567f7efc07a4522d58626fd4049
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-04-19 16:17:55 -0400

    Thursday - Intermediate Lab Complete

--- Wednesday/js-interactivity/index.js
+++ Wednesday/js-interactivity/index.js
@@ -25,13 +25,21 @@ document.querySelector(`form`).addEventListener(`submit`, addMovie);
 
 const deleteMovie = event => {
     event.target.parentNode.remove();
-    message.textContent = `The movie has been deleted!`;
+    message.textContent = `${event.target.previousSibling.textContent} has been deleted!`;
+    revealMessage();
 }
 
 const crossOffMovie = event => {
     event.target.classList.toggle(`checked`);
     if(event.target.classList.contains(`checked`)) {
-        message.textContent = `Movie has been watched.`
+        message.textContent = `${event.target.textContent} has been watched.`
     }
-    else message.textContent = `Movie is on the watch list again.`
+    else message.textContent = `${event.target.textContent} is on the watch list again.`
+}
+
+function revealMessage() {
+    message.classList.remove(`hide`);
+    setTimeout(() => {
+        message.classList.add(`hide`);
+    }, 1000);
 }
